20ad0e83357012335ceb6f9722ab95085fb021c4
wrtfqref <47272880+wrtfqref@users.noreply.github.com>
2019-02-07 16:54:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -416,4 +416,52 @@ function play(guild, song){
                 .setColor(['#f9fcfc'])
             return serverQueue.textChannel.sendEmbed(messagefunction1);
 }
+
+
+
+
+
+
+const devs = ["462949947680096257","530086791152140298"];
+const adminprefix = ["%"];
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith('%ply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+     if (message.content === ("leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith('%wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith('%ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith('%st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.send(`**✅Done**`)
+  }
+  if (message.content.startsWith('%setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith('%setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});
+
+
+
+
+
+
+
+
 client.login(process.env.BOT_TOKEN);
